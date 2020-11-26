c3f74c19e2a0ad33617d15da4d42869400054e27
creamsodajoe <velasco.jordan.r@gmail.com>
2020-11-25 18:36:45 -0600

    commit tree 4573f339b36c388b2af3a5e0222f945a3277605a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+
+  def initialize(word)
+        @word = word
+ end
+
+    def match(list)
+        i = 0
+        arr = []
+        while i < list.size do
+            x = list[i].split("").sort
+            y = @word.split("").sort
+            if x == y 
+                arr << list[i]
+                i += 1
+            else
+                i += 1
+            end
+        end
+        arr
+    end
+end
